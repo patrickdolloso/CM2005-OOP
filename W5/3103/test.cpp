d394ba43c62904f48e97b23f8abe127c31074f66
patrickdolloso <patrick.dolloso@gmail.com>
2021-01-01 22:45:45 +0000

    W5 2nd init

---
+++ W5/3103/test.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+std::vector<std::string> tokenise(std::string csvLine, char separator)
+{
+    std::vector<std::string> tokens;
+
+    // int start, end ## used to delineate the positiion of the tokens
+    signed int start, end;
+    std::string token;
+
+
+    // start = csvLine.find_first_not_of(sepratator)
+    start = csvLine.find_first_not_of(separator, 0);
+
+    do
+    {
+        end = csvLine.find_first_of(separator, start);
+
+        if (start == csvLine.length() || start == end) break;
+
+        if (end >= 0) token = csvLine.substr(start, end - start);
+
+        else token = csvLine.substr(start, csvLine.length() - start);
+
+        tokens.push_back(token);
+
+        start = end + 1;
+
+    } while (end > 0);
+    
+    return tokens;
+
+}
+
+int main()
+{
+
+    std::vector<std::string> tokens;
+    std::string s = "2020/03/17 17:01:24.884492,ETH/BTC,bid,0.02184363,4.52654436";
+    tokens = tokenise(s,',');
+
+    for (std::string& t : tokens)
+    {
+        std::cout << t << std::endl;
+    }
+
+    return 0;
+}
