3520f491a970af50f463fb9f45323caf49c0480e
patrickdolloso <patrick.dolloso@gmail.com>
2021-01-01 03:36:40 +0000

    W4

---
+++ W4/2505/MerkelMain.cpp
@@ -0,0 +1,117 @@
+#include "MerkelMain.h"
+#include <iostream>
+
+MerkelMain::MerkelMain()
+{
+    
+}
+
+void MerkelMain::init()
+{
+
+}
+
+void MerkelMain::printMenu()
+
+{
+
+    // 1 print help
+    std::cout << "1: print help" << std::endl;
+
+    // 2 print exchange stats
+    std::cout << "2: print exchange stats" << std::endl;
+
+    // 3 make offer
+    std::cout << "3: male offer" << std::endl;
+
+    // 4 make bid
+    std::cout << "4: make bid" << std::endl;
+
+    // 5 print wallet
+    std::cout << "5: print wallet" << std::endl;
+
+    // 6 continue
+    std::cout << "6: continue" << std::endl;
+
+
+}
+
+void MerkelMain::printHelp()
+{
+        std::cout << "Help - your aim is to make money. " << std::endl;
+}
+
+void MerkelMain::printMarketStats()
+{
+    std::cout << "Help - your aim is to make money. " << std::endl;
+}
+
+void MerkelMain::enterOffer()
+{
+    std::cout << "Help - your aim is to make money. " << std::endl;
+}
+
+void MerkelMain::enterBid()
+{
+    std::cout << "Help - your aim is to make money. " << std::endl;
+}
+
+void MerkelMain::printWallet()
+{
+    std::cout << "Invalid" << std::endl;
+}
+
+void MerkelMain::gotoNextTimeFrame()
+{
+    std::cout << "Invalid" << std::endl;
+}
+
+int MerkelMain::getUserOption()
+{
+    int userOption;
+    std::cout << "***Type in 1-6***" << std::endl;  
+    std::cin >> userOption;
+    std::cout << "You Chose:" << userOption << std::endl;
+    return userOption;   
+}
+
+void MerkelMain::processUserOption(int userOption)
+
+{
+    if(userOption == 0) // bad input
+    {
+        std::cout << "Invalid" << std::endl;
+    }
+
+    if(userOption == 1) // bad input
+    {
+        printHelp();
+    }
+
+    if(userOption == 2) // bad input
+    {
+        printMarketStats();
+    }
+
+    if(userOption == 3) // bad input
+    {
+        enterOffer();
+    }
+
+    if(userOption == 4) // bad input
+    {
+        enterBid();
+    }
+
+    if(userOption == 5) // bad input
+    {
+        printWallet();
+    }
+
+
+    if(userOption == 6) // bad input
+    {
+        gotoNextTimeFrame();
+    }
+
+}
--- W4/2505/main.cpp
+++ W4/2505/main.cpp
@@ -2,186 +2,49 @@
 #include <string>
 #include <vector>
 #include "OrderBookEntry.h"
+#include "MerkelMain.h"
 
-void printMenu()
-
-{
-
-    // 1 print help
-    std::cout << "1: print help" << std::endl;
-
-    // 2 print exchange stats
-    std::cout << "2: print exchange stats" << std::endl;
-
-    // 3 make offer
-    std::cout << "3: male offer" << std::endl;
-
-    // 4 make bid
-    std::cout << "4: make bid" << std::endl;
-
-    // 5 print wallet
-    std::cout << "5: print wallet" << std::endl;
-
-    // 6 continue
-    std::cout << "6: continue" << std::endl;
-
-
-}
-
-void printHelp()
-{
-        std::cout << "Help - your aim is to make money. " << std::endl;
-}
-
-void printMarketStats()
-{
-    std::cout << "Help - your aim is to make money. " << std::endl;
-}
-
-void enterOffer()
-{
-    std::cout << "Help - your aim is to make money. " << std::endl;
-}
-
-void enterBid()
-{
-    std::cout << "Help - your aim is to make money. " << std::endl;
-}
-
-void printWallet()
-{
-    std::cout << "Invalid" << std::endl;
-}
-
-void gotoNextTimeFrame()
-{
-    std::cout << "Invalid" << std::endl;
-}
-
-int getUserOption()
-{
-    int userOption;
-    std::cout << "***Type in 1-6***" << std::endl;  
-    std::cin >> userOption;
-    std::cout << "You Chose:" << userOption << std::endl;
-    return userOption;   
-}
-
-void processUserOption(int userOption)
-
-{
-    if(userOption == 0) // bad input
-    {
-        std::cout << "Invalid" << std::endl;
-    }
-
-    if(userOption == 1) // bad input
-    {
-        printHelp();
-    }
-
-    if(userOption == 2) // bad input
-    {
-        printMarketStats();
-    }
-
-    if(userOption == 3) // bad input
-    {
-        enterOffer();
-    }
-
-    if(userOption == 4) // bad input
-    {
-        enterBid();
-    }
-
-    if(userOption == 5) // bad input
-    {
-        printWallet();
-    }
-
-
-    if(userOption == 6) // bad input
-    {
-        gotoNextTimeFrame();
-    }
-
-}
 
 int main()
 {
-
-    // double price = 5319.450228;
-    // double amount = 0.00020075;
-
-    // std::string timestamp{"2020/03/17 17:01:24.884492"};
-    // std::string product{"BTC/USDT"};
-    // std::string orderType{"bid"};
-    // OrderBookType ordertype = OrderBookType::bid;
-
-    // std::vector<double> prices;
-    // std::vector<double> amounts;
-    // std::vector<std::string> timestamps;
-    // std::vector<std::string> products;
-    // std::vector<OrderBookType> orderTypes;
-
-    // prices.push_back(5000.01);
-    // amounts.push_back(0.001);
-    // timestamps.push_back("2020/03/17 17:01:24.884492");
-    // products.push_back("BTC/USDT");
-    // orderTypes.push_back(OrderBookType::bid);
-
-    // prices.push_back(10.01);
-    // amounts.push_back(0.001);
-    // timestamps.push_back("2020/03/17 17:01:24.884492");
-    // products.push_back("BTC/USDT");
-    // orderTypes.push_back(OrderBookType::ask);
-
-    // std::cout << "prices: " << prices[0] << std::endl;
-    // std::cout << "prices: " << prices[1] << std::endl;
-
-
-    // while(true)
-    // {
-    // printMenu();
-    // int userOption = getUserOption();
-    // processUserOption(userOption);
-    // }
-
-    std::vector<OrderBookEntry> orders;
-
-    // OrderBookEntry order1{10000, 0.02, "2020/03/17 17:01:24.884492", "BTC/USDT", OrderBookType::bid};
-
-    orders.push_back(   OrderBookEntry{
-        1000,
-        0.02,
-        "2020/03/17 17:01:24.884492", 
-        "BTC/USDT", 
-        OrderBookType::bid}    );
-    
-    orders.push_back(   OrderBookEntry{
-        2000,
-        0.02,
-        "2020/03/17 17:01:24.884492", 
-        "BTC/USDT", 
-        OrderBookType::bid}    );
-
-    std::cout << "The price is: " << orders[0].price << std::endl;
-
-    for(OrderBookEntry& order: orders)
-    {
-    std::cout << "The price is: " << order.price << std::endl;
-    }
-
-    for(unsigned int i = 0; i < orders.size(); ++i)
-    {
-    std::cout << "The price is: " << orders[i].price << std::endl;
-    }
-
-    for(unsigned int i = 0; i < orders.size(); ++i)
-    {
-    std::cout << "The price is: " << orders.at(i).price << std::endl;
-    }
-
-    return 0;
+    MerkelMain app{};
+    app.printMenu();
 }
+
+//     std::vector<OrderBookEntry> orders;
+
+//     // OrderBookEntry order1{10000, 0.02, "2020/03/17 17:01:24.884492", "BTC/USDT", OrderBookType::bid};
+
+//     orders.push_back(   OrderBookEntry{
+//         1000,
+//         0.02,
+//         "2020/03/17 17:01:24.884492", 
+//         "BTC/USDT", 
+//         OrderBookType::bid}    );
+    
+//     orders.push_back(   OrderBookEntry{
+//         2000,
+//         0.02,
+//         "2020/03/17 17:01:24.884492", 
+//         "BTC/USDT", 
+//         OrderBookType::bid}    );
+
+//     std::cout << "The price is: " << orders[0].price << std::endl;
+
+//     for(OrderBookEntry& order: orders)
+//     {
+//     std::cout << "The price is: " << order.price << std::endl;
+//     }
+
+//     for(unsigned int i = 0; i < orders.size(); ++i)
+//     {
+//     std::cout << "The price is: " << orders[i].price << std::endl;
+//     }
+
+//     for(unsigned int i = 0; i < orders.size(); ++i)
+//     {
+//     std::cout << "The price is: " << orders.at(i).price << std::endl;
+//     }
+
+//     return 0;
+// }
